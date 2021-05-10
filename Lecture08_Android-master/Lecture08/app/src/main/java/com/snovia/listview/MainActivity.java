aa23ec411618022479d12089e1df8bc24f69afce
Nooria <bsef18a041@pucit.edu.pk>
2021-05-10 13:09:28 +0500

    done

---
+++ Lecture08_Android-master/Lecture08/app/src/main/java/com/snovia/listview/MainActivity.java
@@ -0,0 +1,64 @@
+package com.snovia.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> friendArrayList;
+    ArrayAdapter<String> arrayAdapter;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.myListView);
+        Button button=findViewById(R.id.btnAdd);
+        editText =findViewById(R.id.editTextAdd);
+
+
+        String[] friendList= {"Ali","Musa","Saad","Zia","Arslan"};
+        friendArrayList= new ArrayList<String>();
+        friendArrayList.add("Ali");
+        friendArrayList.add("Babar");
+        friendArrayList.add("Shahid");
+        friendArrayList.add("Yasir");
+        friendArrayList.add("Abid");
+        friendArrayList.add("Arslan");
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,friendArrayList);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("You clicked",friendArrayList.get(position));
+                Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+                intent.putExtra("Friend Name",friendArrayList.get(position));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void AddFriend(View view) {
+        friendArrayList.add(editText.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
+        Collections.sort(friendArrayList);
+    }
+}
